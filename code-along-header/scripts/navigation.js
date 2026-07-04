120b48a96495e04093f57148eaba66b9d918ab9a
miguelerian <mavalospriante@byupathway.edu>
2026-07-03 18:52:24 -0600

    learning activities

---
+++ code-along-header/scripts/navigation.js
@@ -0,0 +1,9 @@
+const navbuttom = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+
+
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+
+});
