2a077b1053e944f0f35537055846bd53889ddc0d
miguelerian <mavalospriante@byupathway.edu>
2026-07-22 22:02:25 -0600

    week 3 learning activities

---
+++ scripts/weather.js
@@ -0,0 +1,41 @@
+// Select HTML elements
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+// OpenWeatherMap URL
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=a903f278043794483b7e00e290e22a66";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json();
+
+            console.log(data); // Testing
+
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+
+    captionDesc.textContent = desc;
+}
+
+apiFetch();
